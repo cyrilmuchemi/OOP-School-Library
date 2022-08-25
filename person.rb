b700615587e918912cdd92dbb168505e0cc9477a
[cyrilmuchemi] <[cyrilmuchemi@gmail.com]>
2022-08-25 11:27:27 +0300

    Create classroom, book, & rental classes

--- person.rb
+++ person.rb
@@ -1,29 +1,32 @@
 require_relative 'nameable'
+require_relative 'rental'
 
 class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @rentals = []
     @parent_permission = parent_permission
   end
-  attr_reader :id
-  attr_accessor :name, :age
 
   private
-
   def of_age?
     @age >= 18
   end
-
   public
-
   def can_use_services?
     of_age? || @parent_permission
   end
-
   def correct_name
     @name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 end
